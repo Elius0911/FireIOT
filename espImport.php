ce633d7357e278d76ebd7aa92a2210f25691d6f1
Zypheus <c25887@gmail.com>
2022-06-07 21:59:38 +0800

    Full pack can Update

--- espImport.php
+++ espImport.php
@@ -1,23 +1,31 @@
 <?php
 require_once 'database.php';
 
+$sensors = ["Kitchen_fire",
+            "Kitchen_smoke",
+            "Kitchen_people",
+         ];
+
 if(isset($_GET["DataPack"])) {
    $DataPack = $_GET["DataPack"];
    $Datas = mb_split(",", $DataPack);
    print_r($Datas);
-   print($Datas[0]);
-
-   $sql = "UPDATE `sensor` SET `Kitchen_fire` = " .  $Datas[0] . " WHERE " . "1";
-
-   if ($connection->query($sql) === TRUE) {
-      echo "New record created successfully";
-   } else {
-      echo "Error: " . $sql . " => " . $connection->error;
-   }
-
-   $connection->close();
 }
 else {
    echo "Error : (";
 }
+
+function Update ($sensor, $data, $connec) {
+   for ($index = 0; $index <= count($data); $index++) {
+      $sql = "UPDATE `sensor` SET `" . $sensor[$index] . "` = " . $data[$index] . " WHERE 1";
+      if ($connec->query($sql) === TRUE) {
+         echo "New record created successfully";
+      } else {
+         echo "Error: " . $sql . " => " . $connec->error;
+      }
+   }
+}
+
+Update($sensors, $Datas, $connection);
+$connection->close();
 ?>
