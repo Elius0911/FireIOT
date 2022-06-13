f94e6dcc97d4e1abc8474f86054693133fdcff74
Zypheus <c25887@gmail.com>
2022-06-13 16:45:08 +0800

    css Import

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!doctype html>
     <html lang = "zh-Hant-TW">
+    <head>
+        <link rel="stylesheet" href="haha.css">
+    </head>
 
 <body>
     <h1>
         太水了啦
     </h1>
+<<<<<<< Updated upstream
     <img src="./picture/floorPlan.jpg">
+=======
+    <div class="cc">
+        <p>haha</p>
+    </div>
+>>>>>>> Stashed changes
 </body>
 
 <?php
