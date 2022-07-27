afd9a72ec4c6e742c4a059f16369c90630c4c8f6
HSBroJiaJun <axiu0724@gmail.com>
2022-07-27 15:27:54 +0800

    123

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
     <html lang = "zh-Hant-TW">
 
 <body>
+<<<<<<< Updated upstream
     <h1>
         太水了啦
     </h1>
     <img src="./picture/floorPlan.jpg">
+=======
+    <div class="wrapper">
+        <div class="one">One</div>
+        <div class="two">Two</div>
+        <div class="three">Three</div>
+        <div class="four">Four</div>
+        <div class="five">Five</div>
+        <div class="six">Six</div>
+    </div>
+>>>>>>> Stashed changes
 </body>
 
 <?php
