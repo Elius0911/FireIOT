4118c04f58fc4c95d35649441cb24af0c26eaa9c
HSBroJiaJun <qiu7112483@gmail.com>
2022-09-21 12:08:46 +0800

    主網頁並邏輯結果

--- index.php
+++ index.php
@@ -17,31 +17,87 @@ require_once 'logic.php';
 
     <div class="floor">
         <?php
-        echo '<span class="Kitchen_fire"><img src = "picture/redCircle.png" width="50" height="50" >';
-        if ($Kitchen_fire == 1)
-            echo '<span class="Kitchen_fire"><img src = "picture/red.png" width="50" height="50" /></span>';?>
-        <span class="Kitchen_body"><img src="picture/greenCircle.png" width="50" height="50" ></span>
-        <span class="Kitchen_smoke"><img src="picture/orangeCircle.png" width="50" height="50" ></span>
-        <span class="Toilet_body"><img src="picture/greenCircle.png" width="50" height="50" ></span>
-        <span class="Toilet_CO"><img src="picture/blueCircle.png" width="50" height="50" ></span>
-        <span class="KitchenGate_fire"><img src="picture/redCircle.png" width="50" height="50" ></span>
-        <span class="KitchenGate_smoke"><img src="picture/orangeCircle.png" width="50" height="50" ></span>
-        <span class="Livingroom_smoke"><img src="picture/orangeCircle.png" width="50" height="50" ></span>
-        <span class="Livingroom_fire"><img src="picture/redCircle.png" width="50" height="50" ></span>
-        <span class="Gate_smoke"><img src="picture/orangeCircle.png" width="50" height="50" ></span>
-        <span class="Livingroom_body"><img src="picture/greenCircle.png" width="50" height="50" ></span>
-        <span class="Bedroom_smoke"><img src="picture/orangeCircle.png" width="50" height="50" ></span>
-        <span class="Bedroom_body"><img src="picture/greenCircle.png" width="50" height="50" ></span>
-        <span class="Bedroom_fire"><img src="picture/redCircle.png" width="50" height="50" ></span>
+        if ($Kitchen_fire == 0)
+            echo '<span class="Kitchen_fire"><img src = "picture/redCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Kitchen_fire"><img src = "picture/red.png" width="50" height="50" /></span>';
+
+        if ($Kitchen_smoke == 0)
+            echo '<span class="Kitchen_smoke"><img src = "picture/orangeCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Kitchen_smoke"><img src = "picture/orange.png" width="50" height="50" /></span>';
+
+        if ($Kitchen_body == 0)
+            echo '<span class="Kitchen_body"><img src = "picture/greenCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Kitchen_body"><img src = "picture/green.png" width="50" height="50" /></span>';
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        
+        if ($KitchenGate_fire == 0)
+            echo '<span class="KitchenGate_fire"><img src = "picture/redCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="KitchenGate_fire"><img src = "picture/red.png" width="50" height="50" /></span>';
+
+        if ($KitchenGate_smoke == 0)
+            echo '<span class="KitchenGate_smoke"><img src = "picture/orangeCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="KitchenGate_smoke"><img src = "picture/orange.png" width="50" height="50" /></span>';
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        if ($Bedroom_fire == 0)
+            echo '<span class="Bedroom_fire"><img src = "picture/redCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Bedroom_fire"><img src = "picture/red.png" width="50" height="50" /></span>';
+
+        if ($Bedroom_smoke == 0)
+            echo '<span class="Bedroom_smoke"><img src = "picture/orangeCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Bedroom_smoke"><img src = "picture/orange.png" width="50" height="50" /></span>';
+
+        if ($Bedroom_body == 0)
+            echo '<span class="Bedroom_body"><img src = "picture/greenCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Bedroom_body"><img src = "picture/green.png" width="50" height="50" /></span>';
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        if ($Livingroom_fire == 0)
+            echo '<span class="Livingroom_fire"><img src = "picture/redCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Livingroom_fire"><img src = "picture/red.png" width="50" height="50" /></span>';
+
+        if ($Livingroom_smoke == 0)
+            echo '<span class="Livingroom_smoke"><img src = "picture/orangeCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Livingroom_smoke"><img src = "picture/orange.png" width="50" height="50" /></span>';
+
+        if ($Livingroom_body == 0)
+            echo '<span class="Livingroom_body"><img src = "picture/greenCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Livingroom_body"><img src = "picture/green.png" width="50" height="50" /></span>';
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+            
+        if ($Gate_smoke == 0)
+            echo '<span class="Gate_smoke"><img src = "picture/orangeCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Gate_smoke"><img src = "picture/orange.png" width="50" height="50" /></span>';
+
+        if ($Toilet_CO == 0)
+            echo '<span class="Toilet_CO"><img src = "picture/blueCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Toilet_CO"><img src = "picture/blue.png" width="50" height="50" /></span>';
+
+        if ($Toilet_body == 0)
+            echo '<span class="Toilet_body"><img src = "picture/greenCircle.png" width="50" height="50" ></span>';
+        else
+            echo '<span class="Toilet_body"><img src = "picture/green.png" width="50" height="50" /></span>';
+        ?>
         <img src="picture/floorPlan.jpg" width="1000" height="730" style="float:left;margin:0px 50px 0px 180px" alt="">
     </div>
     
-    
-
-        /*echo '<img src = "picture/redCircle.png">';
-        if ($Kitchen_smoke == 1)
-            echo '<div id="KS_flash"><img src = "picture/red.png"/></div>';*/
-    ?>
     <right><a><font size="8">燈號說明：<br><span style="color:red">● 紅色</span>：火焰感測器<br><span style="color:orange">● 橘色</span>：煙霧感測器<br><span style="color:blue">● 藍色</span>：一氧化碳感測器<br><span style="color:green">● 綠色</span>：人體感測器</font></a></right>
 </body>
 
